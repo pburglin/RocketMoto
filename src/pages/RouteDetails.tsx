f51e0b98cfe0c79061f42cdc0d867728fd4ee2c6
Pedro Burglin <pedro.burglin@gmail.com>
2025-02-22 20:59:32 -0700

    minor change

--- src/pages/RouteDetails.tsx
+++ src/pages/RouteDetails.tsx
@@ -220,7 +220,7 @@ export function RouteDetails() {
   if (loading) {
     return (
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="text-center">Loading...</div>
+        <div className="text-center dark:text-gray-200">Loading...</div>
       </div>
     );
   }
